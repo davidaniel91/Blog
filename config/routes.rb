22bbfc48e065f7e36c18fc33715a52ab64422ff1
davidaniel91@gmail.com <davidaniel91@gmail.com>
2017-04-25 13:20:45 -0400

    Finalize google sign in functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destory', as: 'signout'
+  
+  resources :sessions, only: [:create, :destroy]
+  
+  get 'sessions/create'
+
+  get 'sessions/destory'
+
   get 'welcome/index'
   
   resources :articles do
